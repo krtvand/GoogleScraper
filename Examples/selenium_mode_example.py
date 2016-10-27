f7a26222f8221d6ef196851403d20baa4bfa24cb
Andrey Kartaev <krtvand@gmail.com>
2016-10-27 23:26:08 +0300

    fix bug with selenium mode

--- Examples/selenium_mode_example.py
+++ Examples/selenium_mode_example.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from GoogleScraper import scrape_with_config, GoogleSearchError
+from GoogleScraper import database
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine, UniqueConstraint
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
 
 keywords = ['site:*.altsovet.ru -www.altsovet.ru']
 
@@ -22,3 +30,19 @@ config = {
 }
 
 search = scrape_with_config(config)
+def write_to_file():
+    engine = create_engine('sqlite:///' + 'google_scraper.db')
+
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+    ds = set()
+    for q in s.query(database.Link).all():
+        ds.add(q.domain)
+
+    print(len(ds))
+
+    with open('gov_sd', 'w') as f:
+        for d in ds:
+            f.write(d + '\n')
+
