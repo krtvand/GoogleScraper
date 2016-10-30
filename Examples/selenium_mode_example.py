ed1896beaccec9d281490d863ac95f5fb431a6f8
Andrey Kartaev <krtvand@gmail.com>
2016-10-30 23:36:05 +0300

    start with random proxy

--- Examples/selenium_mode_example.py
+++ Examples/selenium_mode_example.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import random
 from GoogleScraper import scrape_with_config, GoogleSearchError
 from GoogleScraper import database
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,19 +15,27 @@ keywords = ['site:*.altsovet.ru -www.altsovet.ru']
 
 # See in the config.cfg file for possible values
 config = {
-    'use_own_ip': True,
+    'use_own_ip': False,
     #'keywords': keywords,
     'keyword_file': '../gov_req.txt',
     'search_engines': 'google',
     'num_pages_for_keyword': 5,
-    'scrape_method': 'http',
-    'sel_browser': 'firefox',
+    'scrape_method': 'selenium',
+    'sel_browser': 'chrome',
     #'clean_cache_files': True,
-    #'proxy_file' : 'proxy',
+    'proxy_file' : 'proxy',
     'check_proxies': False,
+    'output_filename': 'log',
     'print_results': 'summarize'
 
 }
+
+proxies = ['http 194.54.64.90:5093', 'http 194.54.64.90:5094',
+           'http 194.54.64.90:5095', 'http 194.54.64.90:5096',
+           'http 194.54.64.90:5097']
+
+with open('proxy', 'w') as f:
+    f.write(random.choice(proxies))
 
 search = scrape_with_config(config)
 def write_to_file():
