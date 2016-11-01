773b1426935bdfaf8e6347bf56f64c64937981d7
krtvand <krtvand@gmail.com>
2016-11-01 12:54:59 +0300

    write results to file

--- Examples/selenium_mode_example.py
+++ Examples/selenium_mode_example.py
@@ -17,7 +17,7 @@ keywords = ['site:*.altsovet.ru -www.altsovet.ru']
 config = {
     'use_own_ip': False,
     #'keywords': keywords,
-    'keyword_file': '../gov_req.txt',
+    'keyword_file': '/scripts/GoogleScraper/gov_req.txt',
     'search_engines': 'google',
     'num_pages_for_keyword': 5,
     'scrape_method': 'selenium',
@@ -54,3 +54,48 @@ def write_to_file():
         for d in ds:
             f.write(d + '\n')
 
+def add_to_file():
+    engine = create_engine('sqlite:///' + 'google_scraper.db')
+
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+    ds_exist = set()
+    with open('gov_sd', 'r') as f:
+        for d in f:
+            d.rstrip('\n')
+            ds_exist.add(d)
+    print(len(ds_exist))
+
+    for q in s.query(database.Link).all():
+        ds_exist.add(q.domain)
+
+    print(len(ds_exist))
+    with open('gov_sd_all', 'w') as f:
+        for d in ds_exist:
+            f.write(d + '\n')
+
+def select_new_and_write():
+    engine = create_engine('sqlite:///' + 'google_scraper.db')
+
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+    ds_exist = set()
+    with open('gov_sd', 'r') as f:
+        for d in f:
+            d.rstrip('\n')
+            ds_exist.add(d)
+    print(len(ds_exist))
+
+    ds_new = set()
+    for q in s.query(database.Link).all():
+        if q not in ds_exist:
+            ds_new.add(q.domain)
+
+    print(len(ds_new))
+    with open('gov_sd_new', 'w') as f:
+        for d in ds_new:
+            f.write(d + '\n')
+
+#select_new_and_write()
